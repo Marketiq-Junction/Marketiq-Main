a0ad418c7fae509d12c57ee28402a55a751226c9
siddiquiraheem527 <siddiquiraheem527@gmail.com>
2025-11-07 09:23:28 +0530

    Remove unused import and update contact page component reference

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
-import Contact from "@/components/Contact/page";
+// import Contact from "@/components/contact/page";
+import ContactPage from "../../components/contact/page";
 export default function page() {
     return (
         <>
             <Navbar />
-            <Contact />
+            {/* <Contact /> */}
+            <ContactPage/>
             <Footer />  
         </>
     )
